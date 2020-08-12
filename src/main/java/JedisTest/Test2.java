a8444f2ec77b813aeb10b269061b15ac0ba32058
ding <ding@fangzhou.com>
2020-08-12 19:21:24 +0800

    ding-redis-brand2-3

--- src/main/java/JedisTest/Test2.java
+++ src/main/java/JedisTest/Test2.java
@@ -7,7 +7,7 @@ public class Test2 {
     public static void main(String[] args) {
         JedisPool pool = new JedisPool();
         Jedis resource = pool.getResource();
-        resource.set("1","aaa");
+        resource.set("2","ccc");
         resource.set("2","bbb");
         resource.set("1","bbb");
 
